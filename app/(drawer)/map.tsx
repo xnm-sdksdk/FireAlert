d97e068b8a2025e41d1a1463f896f684f7da4278
xnm <n.mendonca@protonmail.com>
2025-12-11 23:26:20 +0000

    add map for android

--- app/(drawer)/map.tsx
+++ app/(drawer)/map.tsx
@@ -1,3 +1,8 @@
+import { GoogleMaps } from "expo-maps";
+import { Platform } from "react-native";
 
 export default function Map() {
+  if (Platform.OS === "android") {
+    return <GoogleMaps.View style={{ flex: 1 }} />;
+  }
 }
