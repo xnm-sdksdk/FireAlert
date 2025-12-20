99860d9bbecab34658564a777fed5910c78499bf
xnm <n.mendonca@protonmail.com>
2025-12-20 17:29:40 +0000

    profile page changes

--- app/(drawer)/profile.tsx
+++ app/(drawer)/profile.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
-import { Button, TextInput, View } from "react-native";
+import {
+  Button,
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 const Profile = () => {
   const {
@@ -13,17 +21,74 @@ const Profile = () => {
   };
   const editData = (data) => {
     console.log(data);
-  }
+  };
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState("");
 
   return (
     <View>
-      <Controller control={control} name="username" render={({ field: {onChange, value}}) => (
-        <TextInput onChangeText={onChange} value={value}/>
-      ) } />
-      <Button title="Edit" onPress={handleSubmit(submitData)}/>
-      <Button title="Submit" onPress={handleSubmit(editData)}/>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="useless placeholder"
+            keyboardType="numeric"
+          />
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <Controller
+        control={control}
+        name="username"
+        render={({ field: { onChange, value } }) => (
+          <TextInput onChangeText={onChange} value={value} />
+        )}
+      />
+
+      <SafeAreaView style={styles.buttonRow}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit(editData)}
+        >
+          <Text>Edit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit(submitData)}
+        >
+          <Text>Submit</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 18,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    width: 150,
+    borderRadius: 18,
+    margin: 10,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  }
+});
 
 export default Profile;
